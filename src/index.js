121b56f3dd42e6338dae69ab5905306e1f731f24
Reza Kelidari <reza.phoenix.1382@gmail.com>
2022-01-27 16:31:37 +0330

    Adding bootstrap-icon library to project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 import App from "./App";
 
 ReactDOM.render(
